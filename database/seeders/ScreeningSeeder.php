eb11bb4cf8a75db07125683dbc84a523658f1ac0
AndreeVillegas <andree.w.villegas@gmail.com>
2026-04-28 21:44:22 -0500

    fix: truncate screenings before seeding

--- database/seeders/ScreeningSeeder.php
+++ database/seeders/ScreeningSeeder.php
@@ -14,6 +14,7 @@ class ScreeningSeeder extends Seeder
      */
     public function run(): void
     {
+	Screening::query()->delete();
         $movies = Movie::all();
         $theaters = Theater::all();
 
